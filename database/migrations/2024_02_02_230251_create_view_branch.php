832ff21f79ba0cdef7988148d315c689b46baf10
Gerardo Gomez <tecnologia6@hostech.com.mx>
2024-02-02 17:59:55 -0600

    CREATE VIEW branch

---
+++ database/migrations/2024_02_02_230251_create_view_branch.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateViewBranch extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement($this->dropView());
+
+        DB::statement($this->createView());
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement($this->dropView());
+    }
+
+    /**
+     * Create view branch
+     * 
+     * @return void
+     */
+    private function createView()
+    {
+        return <<<SQL
+            CREATE VIEW view_brands AS
+                SELECT id, name, description 
+                FROM brands;
+            SQL;
+    }
+
+    /**
+     * Drop view if exists
+     * 
+     * @return void
+     */
+    private function dropView()
+    {
+        return <<<SQL
+            DROP VIEW IF EXISTS view_brands;
+            SQL;
+    }
+}

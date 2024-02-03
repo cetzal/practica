ad15d15d7cec7f40440f7bb5380f7a51a3a04e76
Gerardo Gomez <tecnologia6@hostech.com.mx>
2024-02-02 19:40:20 -0600

    BrandController se aplica en las consultas utilizando view_brand
    Listado principal de marcas se cambia la consulta por BrandView
    
    Listado de marcas se aplica el filtrado por nombre y status:activo
    o inactivo de la marca aplicando validaciones
    
    Creacion de una marca se cambia la regla rule::unique(brands) por
    rule::unique(view_brands)
    
    Obtener una marca se cambia la consulta por la vista(BrandView)
    
    Actualizar de una marca se cambia la regla rule::unique(brands) por
    rule::unique(view_brands)
    
    Desactivar masivamente las marcas se aplicacion validacion y se
    refactoriza
    
    Activar masivamente las marcas se aplicacion validacion y se
    refactoriza

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Brand;
+use App\Models\BrandView;
+use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
 class BrandController extends Controller
@@ -11,28 +12,35 @@ class BrandController extends Controller
 
     public function index()
     {
-        $brand_all = Brand::where('is_active',true)->get();
+        $brand_all = BrandView::where('is_active', true)->get();
         return view('brand.create', compact('brand_all'));
     }
 
-    public function list(){
-        $data = Brand::all();
+    public function list(Request $request){
+        $this->validate($request, [
+            'brand' => ['sometimes', 'required', 'max:255'],
+            'is_active' => ['sometimes', 'boolean']
+        ]);
+
+        $data = BrandView::select(['id', 'name', 'description', 'is_active'])
+                ->filter($request)->get();
+
         return response()->json(['data' =>$data]);
     }
 
     public function store(Request $request)
     {
         $request->name = preg_replace('/\s+/', ' ', $request->name);
-        $validator = $this->validate($request, [
+        $this->validate($request, [
             'name' => [
                 'max:255',
-                Rule::unique('brands')->where(function ($query) {
+                Rule::unique('view_brands')->where(function ($query) {
                     return $query->where('is_active', 1);
                 })
             ],
             'description' => ['required']
         ]);
-        
+
         $input = $request->except('image');
         $input['is_active'] = true;
        
@@ -50,7 +58,8 @@ class BrandController extends Controller
 
     public function edit($id)
     {
-        $brand_data = Brand::findOrFail($id);
+        $brand_data = BrandView::select(['id', 'name', 'description'])
+                        ->findOrFail($id);
         return $brand_data;
     }
 
@@ -59,37 +68,40 @@ class BrandController extends Controller
         $this->validate($request, [
             'name' => [
                 'max:255',
-                    Rule::unique('brands')->ignore($request->brand_id)->where(function ($query) {
+                Rule::unique('view_brands')->ignore($request->brand_id)->where(function ($query) {
                     return $query->where('is_active', 1)->whereNull('deleted_at');
                 }),
             ],
+            'description' => ['required']
         ]);
+
         $brand_data = Brand::findOrFail($request->brand_id);
         $brand_data->name = $request->name;
         $brand_data->description = $request->description;
         $brand_data->save();
+        
         return response()->json(['status' => 'succes', 'message' => 'La marca se guardo con exito']); 
     }
 
     public function desactivarBySelection(Request $request)
     {
-        $brand_id = $request['brandIdArray'];
-        foreach ($brand_id as $id) {
-            $brand_data = Brand::findOrFail($id);
-            $brand_data->is_active = false;
-            $brand_data->save();
-        }
+        $request->validate($request, [
+            'brandIdArray' => ['required', 'array', 'min:1']
+        ]);
+
+        Brand::whereIn('id', $request['brandIdArray'])->update(['is_active' => false]);
+        
         return response()->json(['status' => 'succes', 'message' => 'La marca ha sido desactiva']); 
     }
 
     public function activarBySelection(Request $request)
     {
-        $brand_id = $request['brandIdArray'];
-        foreach ($brand_id as $id) {
-            $brand_data = Brand::findOrFail($id);
-            $brand_data->is_active = true;
-            $brand_data->save();
-        }
+        $request->validate($request, [
+            'brandIdArray' => ['required', 'array', 'min:1']
+        ]);
+
+        Brand::whereIn('id', $request['brandIdArray'])->update(['is_active' => true]);
+
         return response()->json(['status' => 'succes', 'message' => 'La marca ha sido activado']); 
     }
 
--- app/Models/BrandView.php
+++ app/Models/BrandView.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BrandView extends Model
 {
-    use HasFactory;
+    use HasFactory,
+        SoftDeletes;
+
+    protected $table = 'view_brands';
+
+    public function scopeFilter($query, $request)
+    {
+        $where = [];
+        if (!empty($request->brand)) {
+            $where[] = ['name', '=', $request->brand];
+        }
+
+        if (!empty($request->brand)) {
+            $where[] = ['name', '=', $request->brand];
+        }
+
+       return $query->where($where);
+    }
 }

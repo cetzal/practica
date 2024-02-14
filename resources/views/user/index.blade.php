f878358540200f47b4c3401e9eaa0b46f701b156
Gerardo Gomez <gomez.gergil@gmail.com>
2024-02-13 18:00:45 -0600

    cambios nombre de espaniol a ingles

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -73,8 +73,8 @@
                 </div>
                 <div class="col">
                     <label for=""></label>
-                    <button type="submit" class="btn btn-primary mt-4 filter_data">Filtrar</button>
-                    <button type="button" class="btn btn-primary mt-4 clear_form_user">Limpiar</button>
+                    <button type="submit" class="btn btn-primary mt-4 filter_data">Filter</button>
+                    <button type="button" class="btn btn-primary mt-4 clear_form_user">Clear</button>
                     <button type="button" class="btn btn-primary mt-4 close_form">Close</button>
                 </div>
             </div>

658a7acfd4d99e4374e879e41d3c9b29f7bf262e
Gerardo Gomez <gomez.gergil@gmail.com>
2024-02-16 08:35:34 -0600

    Corrige la vista view_clients y se agrego view_products_brands_edit

--- database/sql/views/CREATE-view_products_brands_edit.sql
+++ database/sql/views/CREATE-view_products_brands_edit.sql
@@ -2,5 +2,5 @@ CREATE OR REPLACE VIEW view_products_brands_edit AS
 	SELECT p.brand_id, b.name
 	FROM products p 
 	INNER JOIN brands b ON b.id = p.brand_id
-	WHERE b.is_active = 1 OR b.deleted_at is not NULL
+	WHERE b.is_active = 1 OR b.deleted_at IS NOT NULL
 	GROUP BY p.brand_id, b.name;

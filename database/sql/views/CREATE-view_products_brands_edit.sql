82ae88e8481ca2601fe56f335c3881d416e6a632
Gerardo Gomez <gomez.gergil@gmail.com>
2024-02-16 09:30:26 -0600

    en la vista view_products_brands_edit correccion

--- database/sql/views/CREATE-view_products_brands_edit.sql
+++ database/sql/views/CREATE-view_products_brands_edit.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW view_products_brands_edit AS 
-	SELECT p.brand_id, b.name
+	SELECT p.brand_id AS id, b.name
 	FROM products p 
 	INNER JOIN brands b ON b.id = p.brand_id
 	WHERE b.is_active = 1 OR b.deleted_at IS NOT NULL

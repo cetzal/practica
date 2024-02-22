5824cee69c6b41046c07930f5032901554635d2b
Gerardo Gomez <gomez.gergil@gmail.com>
2024-02-22 17:11:14 -0600

    Crear venta formulario se deshabilida el campo date

--- resources/js/modules/sale/create.js
+++ resources/js/modules/sale/create.js
@@ -15,6 +15,8 @@
         },
     });
 
+    $("#sale_date").prop('disabled', true);
+
     $('#select_supplier').on('change', function() {
         let supplier_id = $(this).val();
         let url = '/api/sales/getBrandsBySupplierId/'+supplier_id;

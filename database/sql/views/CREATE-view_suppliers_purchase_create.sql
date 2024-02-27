07377755332dd3142d6289d6b4f986cda6e0fb1b
Omar cetzal <omarc.developer@hotmail.com>
2024-02-27 10:17:18 -0600

    vista sql bug

--- database/sql/views/CREATE-view_suppliers_purchase_create.sql
+++ database/sql/views/CREATE-view_suppliers_purchase_create.sql
@@ -1,8 +1,6 @@
 CREATE OR REPLACE VIEW view_suppliers_purchase_create AS
-SELECT s.id, s.name
-FROM view_suppliers_active s
-WHERE s.id IN(
-    SELECT vp.supplier_id
-    FROM view_products vp
-    GROUP BY vp.supplier_id
+SELECT vsa.id, vsa.name 
+FROM view_suppliers_active vsa
+WHERE vsa.id in (
+	SELECT vpa.supplier_id FROM view_products_active vpa WHERE vpa.brand_id IN (SELECT vba.id FROM view_brands_active vba WHERE vba.id = vpa.brand_id)
 );

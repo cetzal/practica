ad9b2d95870dc085f4da51a6aeebd9db8cbcf6a5
Gerardo Gomez <gomez.gergil@gmail.com>
2024-03-05 11:38:54 -0600

    Correccion en las vistas para agregar el in

--- database/sql/views/CREATE-view_suppliers_purchase_create.sql
+++ database/sql/views/CREATE-view_suppliers_purchase_create.sql
@@ -4,5 +4,4 @@ FROM view_suppliers_active vsa
 WHERE vsa.id in (
 	SELECT DISTINCT vpa.supplier_id 
 	FROM view_products_active vpa 
-	WHERE vpa.brand_id = (SELECT vba.id FROM view_brands_active vba WHERE vba.id = vpa.brand_id)
-);
+)

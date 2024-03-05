6a57f95c3c7eee10ab23b07dad01747211eaffb2
Gerardo Gomez <gomez.gergil@gmail.com>
2024-03-05 10:45:46 -0600

    Edicion producto en si el proveedor esta desactivado las marcas aun actividas pasan como desactividas

--- database/sql/views/CREATE_view_products_brands_edit.sql
+++ database/sql/views/CREATE_view_products_brands_edit.sql
@@ -1,3 +1,8 @@
 CREATE OR REPLACE VIEW view_products_brands_edit AS
-SELECT b.id, b.name, b.supplier_id, b.is_active
+SELECT b.id, b.name, b.supplier_id, (
+        SELECT IF(s.is_active = 1 &&  b.is_active = 1, 1, 0) 
+        FROM suppliers s 
+        WHERE s.id = b.supplier_id
+    ) as is_active
 FROM brands b
+WHERE b.supplier_id IS NOT NULL;

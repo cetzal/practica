78c77779a0ee1a1078928b662883cf5c1f5b657a
Gerardo Gomez <gomez.gergil@gmail.com>
2024-03-07 09:20:35 -0600

    view_accounts correccion por escritura

--- database/sql/views/CREATE-view_accounts.sql
+++ database/sql/views/CREATE-view_accounts.sql
@@ -4,8 +4,8 @@ SELECT
 	a.name,
     a.init_balance,
 	a.is_active,
-	(SELECT CONCAT(u.name, ' ', u.last_name) FROM users AS u WHERE u.id = c.user_id) as created_by,
+	(SELECT CONCAT(u.name, ' ', u.last_name) FROM users AS u WHERE u.id = a.user_id) as created_by,
 	a.created_at,
-	a.updated_at,
+	a.updated_at
 FROM accounts as a
 WHERE  deleted_at IS NULL;

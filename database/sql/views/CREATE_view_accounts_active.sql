b06b032de243b6bf9cadc8cbfc8004c595cc5b01
Omar cetzal <omarc.developer@hotmail.com>
2024-03-11 11:07:39 -0600

    modificando sql

--- database/sql/views/CREATE_view_accounts_active.sql
+++ database/sql/views/CREATE_view_accounts_active.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW view_accounts_active AS
-SELECT id, name, init_balance, is_active, created_by, created_at, updated_at
-FROM view_accounts
-WHERE is_active = 1;
+SELECT va.id, va.name, va.init_balance, va.is_active, va.created_by, va.revenue, va.egress, va.balance, va.created_at, va.updated_at
+FROM view_accounts AS va
+WHERE va.is_active = 1;
